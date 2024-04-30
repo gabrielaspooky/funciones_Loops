22a0fe561dbf1068d0906e258288f0b1c39ec686
Gabriela <54961860+gabrielaspooky@users.noreply.github.com>
2024-04-30 21:11:49 +0200

    added step by step

---
+++ funciones.js
@@ -0,0 +1,13 @@
+// 1.- Escribe una función que acepte la medida de los dos lados de un rectángulo y devuelva el área de dicho rectángulo. Si no se le pasa ningún parámetro, los lados deberán ser, por defecto, 5 y 3. Puedes usar prompts para los datos (opcional) y la consola del navegador para comprobar el resultado.
+
+// 2.- Escribe una función que acepte grados Celsius y devuelva la conversión a grados Fahrenheit.
+
+// 3.- Escribe una función que acepte dos números, un mínimo y un máximo, y devuelva un número aleatorio entre dichos números. Para ello, tendrás que descubrir cómo funciona el método Math.random().
+
+// 4.- Escribe una función que calcule el área de un círculo si se le proporciona el radio.
+
+// 5.- Escribe una función que te adivine el futuro. Recibirá como parámetros:
+// Número de hijos.
+// Nombre de la pareja.
+// Nombre de una ciudad.
+// Un trabajo. La función devolverá "Trabajarás en <<ciudad>>, trabajando como <<trabajo>>, y tendrás <<número de hijos>> hijos con <<pareja>>.". Deberás usar template literals para ello.
---
+++ loops.js
@@ -0,0 +1,61 @@
+
+// 1.- 
+//         1.1.- Itera del 0 al 10 usando un bucle "for" e imprime cada número por consola.
+//         1.2.-  Haz lo mismo con un bucle "while".
+//         1.3.- Haz lo mismo del 83 al 24 (bucle "for" y bocle "while").
+
+// 2.- Crea la variable "num" y haz que un bucle "for" imprima la tabla de multiplicar de ese número (del 0 al 10) en la consola, siguiendo el siguiente formato:
+
+// num x 0 = 0
+// num x 1 = num * 1
+// num x 2 = num * 2
+// etc...
+
+// 3.- Imprime por consola el siguiente patrón:
+//  i    i**2   i**3
+//  ----------------- Lo de abajo---------------
+//  0    0     0
+//  1    1     1
+//  2    4     8
+//  3    9     27
+//  4    16    64
+//  5    25    125
+//  6    36    216
+//  7    49    343
+//  8    64    512
+//  9    81    729
+//  10   100   1000
+
+
+// 4.- Imprime por consola, , la suma de todos los números del 0 al 10.
+
+// 5.-
+//          5.1.- Imprime por consola los números del 0 hasta el 100, pero sólo los que estén comprendidos entre el 10 y 23, entre el 56 y 62.
+//          5.2.- Luego haz otro log que muestre solo los números divisibles entre 5 o 7.
+
+// 6.- Pídele al usuario que introduzca un número e imprime por consola: "La suma de todos los números pares del 0 al <<num>> es <<result>>, y la suma de todos los números impares del 0 al <<num>> es <<result>>."
+
+// 7.- Imprime el siguiente patrón por consola:
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+// 8.- Imprime por consola el siguiente patrón:
+//                ##
+//             ####
+//          ######
+//        ########
+//      ##########
+//    ############
+// ##############
+//   ############
+//     ##########
+//       ########
+//        ######
+//          ####
+//            ##
