ca02a48b3a8e7d72cf8f71c7ec9509bcc548ea4a
Gabriela <54961860+gabrielaspooky@users.noreply.github.com>
2024-05-01 18:26:11 +0200

    added temperatureConverter

--- funciones.js
+++ funciones.js
@@ -2,12 +2,22 @@
 
 // 2.- Escribe una función que acepte grados Celsius y devuelva la conversión a grados Fahrenheit.
 
-// 3.- Escribe una función que acepte dos números, un mínimo y un máximo, y devuelva un número aleatorio entre dichos números. Para ello, tendrás que descubrir cómo funciona el método Math.random().
+function temperatureConverter() 
+{
+let celsiusTemperature = prompt("Dime la temperatura en grados Celsius");
+let fahrenheitTemperature = `${celsiusTemperature}` * 9/5 + 32;
+alert(`${celsiusTemperature}°C es igual a: ${fahrenheitTemperature}°F`)
+}
 
-// 4.- Escribe una función que calcule el área de un círculo si se le proporciona el radio.
 
-// 5.- Escribe una función que te adivine el futuro. Recibirá como parámetros:
-// Número de hijos.
-// Nombre de la pareja.
-// Nombre de una ciudad.
-// Un trabajo. La función devolverá "Trabajarás en <<ciudad>>, trabajando como <<trabajo>>, y tendrás <<número de hijos>> hijos con <<pareja>>.". Deberás usar template literals para ello.
+temperatureConverter()
+
+3.- Escribe una función que acepte dos números, un mínimo y un máximo, y devuelva un número aleatorio entre dichos números. Para ello, tendrás que descubrir cómo funciona el método Math.random().
+
+4.- Escribe una función que calcule el área de un círculo si se le proporciona el radio.
+
+5.- Escribe una función que te adivine el futuro. Recibirá como parámetros:
+Número de hijos.
+Nombre de la pareja.
+Nombre de una ciudad.
+Un trabajo. La función devolverá "Trabajarás en <<ciudad>>, trabajando como <<trabajo>>, y tendrás <<número de hijos>> hijos con <<pareja>>.". Deberás usar template literals para ello.
